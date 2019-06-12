8f74db23e349bf38bd300fe76e144548230ad77d
NastuzziSamy <samy@nastuzzi.fr>
2019-06-12 10:44:35 +0200

    Reorganize the Laramore provider

--- src/Providers/LaramoreLoader.php
+++ src/Providers/LaramoreLoader.php
@@ -22,13 +22,47 @@ use ReflectionNamespace;
 
 class LaramoreLoader extends ServiceProvider
 {
+    /**
+     * Grammar and Model observer managers.
+     *
+     * @var BaseObservableManager
+     */
     protected $grammarObservableManager;
     protected $modelObserverManager;
+
+    /**
+     * Type manager.
+     *
+     * @var TypeManager
+     */
     protected $typeManager;
+
+    /**
+     * Meta manager.
+     *
+     * @var MetaManager
+     */
     protected $metaManager;
+
+    /**
+     * Default grammar namespace.
+     *
+     * @var string
+     */
     protected $grammarNamespace = 'Illuminate\\Database\\Schema\\Grammars';
+
+    /**
+     * Default model namespace.
+     *
+     * @var string
+     */
     protected $modelNamespace = 'App\\Models';
 
+    /**
+     * Default types to create.
+     *
+     * @var array
+     */
     protected $defaultTypes = [
         'boolean',
         'increment',
@@ -42,11 +76,25 @@ class LaramoreLoader extends ServiceProvider
     ];
 
     /**
-     * Prepare all metas and lock them.
+     * Prepare all singletons and add booting and booted callbacks.
      *
      * @return void
      */
     public function register()
+    {
+        $this->createSigletons();
+        $this->createObjects();
+
+        $this->app->booting($this->bootingCallback);
+        $this->app->booted($this->bootedCallback);
+    }
+
+    /**
+     * Create all singletons: GrammarObservableManager, ModelObservableManager, TypeManager, MetaManager.
+     *
+     * @return void
+     */
+    protected function createSigletons()
     {
         $this->app->singleton('GrammarObservableManager', function() {
             return $this->grammarObservableManager;
@@ -63,50 +111,70 @@ class LaramoreLoader extends ServiceProvider
         $this->app->singleton('MetaManager', function() {
             return $this->metaManager;
         });
+    }
 
+    /**
+     * Create all singleton objects: GrammarObservableManager, ModelObservableManager, TypeManager, MetaManager.
+     *
+     * @return void
+     */
+    protected function createObjects()
+    {
         $this->grammarObservableManager = new GrammarObservableManager;
         $this->modelObserverManager = new ModelObservableManager;
         $this->typeManager = new TypeManager($this->defaultTypes);
         $this->metaManager = new MetaManager;
-
-        $this->app->booting($this->bootingCallback());
-        $this->app->booted($this->bootedCallback());
     }
 
     /**
-     * Add all Grammar classes in the GrammarObservableManager.
+     * Add all metas to the MetaManager from a specific namespace.
+     *
+     * @return void
+     */
+    protected function addMetas()
+    {
+        foreach ((new ReflectionNamespace($this->modelNamespace))->getClasses() as $modelClass) {
+            if (\in_array(HasLaramore::class, $modelClass->getTraitNames())) {
+                $this->metaManager->addMeta($modelClass->getName()::getMeta());
+            }
+        }
+    }
+
+    /**
+     * Create grammar observable handlers for each possible grammars and add them to the GrammarObservableManager.
+     *
+     * @return void
+     */
+    protected function createGrammarObservers()
+    {
+        foreach ((new ReflectionNamespace($this->grammarNamespace))->getClassNames() as $class) {
+            if ($this->grammarObservableManager->isObservable($class)) {
+                $this->grammarObservableManager->createObservableHandler($class);
+            }
+        }
+    }
+
+    /**
+     * Prepare metas and grammar observable handlers before booting.
      *
      * @return void
      */
     protected function bootingCallback()
     {
-        return function () {
-            foreach ((new ReflectionNamespace($this->modelNamespace))->getClasses() as $modelClass) {
-                if (in_array(HasLaramore::class, $modelClass->getTraitNames())) {
-                    $this->metaManager->addMeta($modelClass->getName()::getMeta());
-                }
-            }
-
-            foreach ((new ReflectionNamespace($this->grammarNamespace))->getClassNames() as $class) {
-                if ($this->grammarObservableManager->isObservable($class)) {
-                    $this->grammarObservableManager->createObservableHandler($class);
-                }
-            }
-        };
+        $this->addMetas();
+        $this->createGrammarObservers();
     }
 
     /**
-     * Lock all models after the booting is finished.
+     * Lock all managers after booting.
      *
      * @return void
      */
     protected function bootedCallback()
     {
-        return function () {
-            $this->metaManager->lock();
-            $this->typeManager->lock();
-            $this->modelObserverManager->lock();
-            $this->grammarObservableManager->lock();
-        };
+        $this->metaManager->lock();
+        $this->typeManager->lock();
+        $this->modelObserverManager->lock();
+        $this->grammarObservableManager->lock();
     }
 }
