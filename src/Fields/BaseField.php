b57655165b0e9a3f49b65492c3862353bdb7b812
NastuzziSamy <samy@nastuzzi.fr>
2019-07-28 22:30:12 +0200

    Create a Trait for properties management

--- src/Fields/BaseField.php
+++ src/Fields/BaseField.php
@@ -13,94 +13,39 @@ namespace Laramore\Fields;
 use Illuminate\Support\Str;
 use Laramore\Observers\ModelObserver;
 use Laramore\Interfaces\IsAField;
-use Laramore\Traits\IsOwnedAndLocked;
+use Laramore\Traits\{
+    IsOwnedAndLocked, HasProperties
+};
 use Laramore\Meta;
 
 abstract class BaseField implements IsAField
 {
-    use IsOwnedAndLocked;
+    use IsOwnedAndLocked, HasProperties {
+        setProperty as protected forceProperty;
+    }
 
     protected $nullable;
 
-    public function __call(string $method, array $args)
-    {
-
-        if (count($args) === 0) {
-            if (Str::startsWith($method, 'get')) {
-                return $this->getProperty(Str::camel(substr(Str::snake($method), 4)));
-            } else {
-                return $this->setProperty($method, true);
-            }
-        } else if (count($args) === 1) {
-            $this->setProperty($method, $args[0]);
-        } else {
-            $this->setProperty($method, $args);
-        }
-
-        return $this;
-    }
-
-    public function __get(string $key)
-    {
-        return $this->getProperty($key);
-    }
-
-    public function __set(string $key, $value)
-    {
-        return $this->setProperty($key, $value);
-    }
-
-    public function __isset(string $key): bool
-    {
-        return $this->hasProperty($key);
-    }
-
-    public function hasProperty(string $key): bool
-    {
-        return isset($this->$key);
-    }
-
     public function getProperty(string $key)
     {
-        if (property_exists($this, $key)) {
-            if (method_exists($this, $method = 'get'.ucfirst($key))) {
-                return $this->$method();
+        if ($this->hasProperty($key)) {
+            if (\method_exists($this, $method = 'get'.\ucfirst($key))) {
+                return \call_user_func([$this, $method]);
             }
 
             return $this->$key;
-        } else if (defined($const = 'static::'.strtoupper(Str::snake($key)))) {
-            return $this->hasRule(constant($const));
+        } else if (\defined($const = 'static::'.\strtoupper(Str::snake($key)))) {
+            return $this->hasRule(\constant($const));
         }
 
-        return null;
-    }
-
-    protected function defineProperty(string $key, $value)
-    {
-        $this->$key = $value;
-
-        return $this;
+        throw new \Exception("The property $key does not exist");
     }
 
     public function setProperty(string $key, $value)
     {
         $this->needsToBeUnlocked();
 
-        if (method_exists($this, $key)) {
-            call_user_func([$this, $key], $value);
-        } else if (property_exists($this, $key)) {
-            $this->defineProperty($key, $value);
-        } else if (defined($const = 'static::'.strtoupper(Str::snake($key)))) {
-            if ($value) {
-                $this->addRule(constant($const));
-            } else {
-                $this->removeRule(constant($const));
-            }
-        } else {
-            throw new \Exception("The propery $key cannot be set");
-        }
-
-        return $this;
+        return $this->forceProperty($key, $value);
     }
 
     /**
@@ -141,7 +86,7 @@ abstract class BaseField implements IsAField
         return $this;
     }
 
-    public function relationValue($model)
+    public function getRelationValue($model)
     {
         return $this->whereValue($model, $model->{$this->name});
     }
---
+++ src/Traits/HasProperties.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Add a property management.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2019
+ * @license MIT
+ */
+
+namespace Laramore\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasProperties
+{
+    /**
+     * Indicate if a property exists.
+     *
+     * @param  string $key
+     * @return bool
+     */
+    public function hasProperty(string $key): bool
+    {
+        return isset($this->$key);
+    }
+
+    /**
+     * Return a property by its name.
+     *
+     * @param  string $key
+     * @return mixed
+     * @throws \ErrorException If no property exists with this name
+     */
+    public function getProperty(string $key)
+    {
+        if ($this->hasProperty($key)) {
+            if (\method_exists($this, $method = 'get'.\ucfirst($key))) {
+                return \call_user_func([$this, $method]);
+            }
+
+            return $this->$key;
+        }
+
+        throw new \ErrorException("The property $key does not exist");
+    }
+
+    /**
+     * Define a property.
+     *
+     * @param  string $key
+     * @param  mixed $value
+     * @return self
+     */
+    protected function defineProperty(string $key, $value): self
+    {
+        $this->$key = $value;
+
+        return $this;
+    }
+
+    /**
+     * Manage the definition of a property.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return self
+     * @throws \ErrorException If no property exists with this name
+     */
+    public function setProperty(string $key, $value): self
+    {
+        if (\method_exists($this, $key)) {
+            \call_user_func([$this, $key], $value);
+        } else if (\property_exists($this, $key)) {
+            $this->defineProperty($key, $value);
+        } else if (\defined($const = 'static::'.strtoupper(Str::snake($key)))) {
+            if ($value) {
+                $this->addRule(constant($const));
+            } else {
+                $this->removeRule(constant($const));
+            }
+        } else {
+            throw new \ErrorException("The propery $key cannot be set as it does not exist");
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return the value of a property if it exists with a given name.
+     *
+     * @param  string $key
+     * @return mixed
+     * @throws \ErrorException If no property exists with this name
+     */
+    public function __get(string $key)
+    {
+        return $this->getProperty($key);
+    }
+
+    /**
+     * Set the value of a property.
+     *
+     * @param  string $key
+     * @param mixed $value
+     * @throws \ErrorException If no property exists with this name
+     * @return mixed
+     */
+    public function __set(string $key, $value)
+    {
+        return $this->setProperty($key, $value);
+    }
+
+    /**
+     * Indicate if a property exists.
+     *
+     * @param  string  $key
+     * @return boolean
+     */
+    public function __isset(string $key): bool
+    {
+        return $this->hasProperty($key);
+    }
+
+    /**
+     * Return a property, or set one.
+     *
+     * @param  string $method
+     * @param  array  $arg
+     * @return mixed
+     */
+    public function __call(string $method, array $args)
+    {
+
+        if (count($args) === 0) {
+            if (Str::startsWith($method, 'get')) {
+                return $this->getProperty(Str::camel(substr(Str::snake($method), 4)));
+            } else {
+                return $this->setProperty($method, true);
+            }
+        } else if (count($args) === 1) {
+            $this->setProperty($method, $args[0]);
+        } else {
+            $this->setProperty($method, $args);
+        }
+
+        return $this;
+    }
+}
