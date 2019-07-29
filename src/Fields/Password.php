d632b125b3db5ab8af0d16312bc8ba8544800466
NastuzziSamy <samy@nastuzzi.fr>
2019-07-29 15:10:12 +0200

    Fully use the rule strategy for password fields

--- src/Fields/Password.php
+++ src/Fields/Password.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Hash;
 class Password extends Pattern
 {
     protected $minLength = 8;
-    protected $lowercase = true;
-    protected $uppercase = true;
-    protected $number = true;
-    protected $special = false;
 
     public const REGEX_MIN_MAX_CARACTER = '(?=\S{$min,$max})';
     public const REGEX_AT_LEAST_ONE_LOWERCASE = '(?=\S*[a-z])';
@@ -26,8 +22,26 @@ class Password extends Pattern
     public const REGEX_AT_LEAST_ONE_NUMBER = '(?=\S*[\d])';
     public const REGEX_AT_LEAST_ONE_SPECIAL = '(?=\S*[\W])';
 
+    // Need one lowercase caracter at least.
+    public const NEED_ONE_LOWERCASE = 131072;
+
+    // Need one uppercase caracter at least.
+    public const NEED_ONE_UPPERCASE = 262144;
+
+    // Need one number caracter at least.
+    public const NEED_ONE_NUMBER = 524288;
+
+    // Need one special caracter at least.
+    public const NEED_ONE_SPECIAL = 1048576;
+
+    // The password length must be at least of the defined length.
+    public const MIN_LENGTH = 2097152;
+
+    // The password length must be at max of the defined length.
+    public const MAX_LENGTH = 4194304;
+
     // Default rules
-    public const DEFAULT_PASSWORD = (self::DEFAULT_TEXT | self::MATCH_PATTERN ^ self::VISIBLE);
+    public const DEFAULT_PASSWORD = (self::DEFAULT_TEXT | self::MATCH_PATTERN | self::NEED_ONE_LOWERCASE | self::NEED_ONE_UPPERCASE | self::NEED_ONE_NUMBER | self::MIN_LENGTH ^ self::VISIBLE);
 
     protected static $defaultRules = self::DEFAULT_PASSWORD;
 
@@ -55,25 +69,25 @@ class Password extends Pattern
     {
         $rules = [];
 
-        if ($this->minLength) {
-            $rules[] = str_replace('$min', $this->minLength,
-                str_replace('$max', $this->length ?: '', static::REGEX_MIN_MAX_CARACTER)
+        if ($this->hasRule(self::MIN_LENGTH) || $this->hasRule(self::MAX_LENGTH)) {
+            $rules[] = str_replace('$min', $this->minLength ?: '',
+                str_replace('$max', $this->maxLength ?: '', static::REGEX_MIN_MAX_CARACTER)
             );
         }
 
-        if ($this->lowercase) {
+        if ($this->hasRule(self::NEED_ONE_LOWERCASE)) {
             $rules[] = static::REGEX_AT_LEAST_ONE_LOWERCASE;
         }
 
-        if ($this->uppercase) {
+        if ($this->hasRule(self::NEED_ONE_UPPERCASE)) {
             $rules[] = static::REGEX_AT_LEAST_ONE_UPPERCASE;
         }
 
-        if ($this->number) {
+        if ($this->hasRule(self::NEED_ONE_NUMBER)) {
             $rules[] = static::REGEX_AT_LEAST_ONE_NUMBER;
         }
 
-        if ($this->special) {
+        if ($this->hasRule(self::NEED_ONE_SPECIAL)) {
             $rules[] = static::REGEX_AT_LEAST_ONE_SPECIAL;
         }
 
