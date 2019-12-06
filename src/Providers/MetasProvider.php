3863110791df3601f768794041e0c1c0c6d71446
NastuzziSamy <samy@nastuzzi.fr>
2019-12-06 02:06:13 +0100

    Merge configs

--- src/Providers/MetasProvider.php
+++ src/Providers/MetasProvider.php
@@ -14,26 +14,20 @@ use Illuminate\Support\ServiceProvider;
 use Laramore\Interfaces\{
 	IsALaramoreManager, IsALaramoreProvider, IsALaramoreModel
 };
+use Laramore\Traits\Provider\MergesConfig;
 use Laramore\Exceptions\ConfigException;
 use ReflectionNamespace;
 
 class MetasProvider extends ServiceProvider implements IsALaramoreProvider
 {
+    use MergesConfig;
+
     /**
      * Meta manager.
      *
      * @var array
      */
     protected static $managers;
-
-    /**
-     * Define all proxy files to merge into config.
-     *
-     * @var array
-     */
-    protected static $commonProxies = [
-        'cast', 'default', 'dry', 'get', 'reset', 'serialize', 'set', 'transform',
-    ];
 
     /**
      * Register our facade and create the manager.
@@ -46,11 +40,9 @@ class MetasProvider extends ServiceProvider implements IsALaramoreProvider
             __DIR__.'/../../config/metas.php', 'metas',
         );
 
-        foreach (static::$commonProxies as $proxy) {
-            $this->mergeConfigFrom(
-            __DIR__."/../../config/fields/proxies/common/$proxy.php", "fields.proxies.common.$proxy",
-            );
-        }
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/fields.php', 'fields',
+        );
 
         $this->app->singleton('Metas', function() {
             return static::getManager();
@@ -64,13 +56,9 @@ class MetasProvider extends ServiceProvider implements IsALaramoreProvider
      */
     public function boot()
     {
-        $this->publishes(\array_merge([
+        $this->publishes([
             __DIR__.'/../../config/metas.php' => config_path('metas.php'),
-        ], ...\array_map(function ($proxy) {
-            return [
-                __DIR__."/../../config/fields/proxies/common/$proxy.php" => config_path("fields/proxies/common/$proxy.php"),
-            ];
-        }, static::$commonProxies)));
+        ]);
 
         $this->app->register(LastProvider::class);
     }
